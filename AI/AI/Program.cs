a0219a6bdba449be0d84a61392c99cc117949312
szymon157 <72827230+szymon157@users.noreply.github.com>
2021-04-20 11:23:59 +0200

    czytanie

--- AI/AI/Program.cs
+++ AI/AI/Program.cs
@@ -12,8 +12,24 @@ namespace AI
     {
         static void Main(string[] args)
         {
+       
 
+            string path = Environment.CurrentDirectory + "/data.json";
+            string json = File.ReadAllText(path);
+           
 
+            var items = json.FromJson<List<Item>>();
+
+            items.ForEach(i => Console.WriteLine(i));
+
+            Console.ReadLine();
+
+           
+
+        }
+
+        private static void Generate()
+        {
             var generator = new Generator(2, 300, 5, 50);
 
             var items = generator.Generatr(200);
@@ -25,8 +41,6 @@ namespace AI
 
 
             items.ForEach(i => Console.WriteLine(i));
-            
-
         }
     }
 }
